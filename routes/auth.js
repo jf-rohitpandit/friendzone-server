d47a49b995adc4f76aec91b3cab9f7dede38dd6a
Rohit Pandit <rohit0311@outlook.com>
2021-04-19 09:12:49 +0530

    adds login route

--- routes/auth.js
+++ routes/auth.js
@@ -6,13 +6,15 @@ const router = express.Router();
 router.post('/signup', async (req, res) => {
 	try {
 		const { email, password } = req.body;
+		console.log(email, password);
 		if (!email || !password) {
-			res.status(400).json({ message: 'Enter Email and Password' });
+			res.status(400).json({ message: 'Enter Email and Password!' });
 			return;
 		}
 
-		const isUser = await User.find({ email });
-		if (isUser) {
+		const isUser = await User.findOne({ email });
+		console.log(isUser);
+		if (isUser !== null) {
 			res
 				.status(400)
 				.json({ message: 'Email already registered! Try another email' });
@@ -23,6 +25,39 @@ router.post('/signup', async (req, res) => {
 		await newUser.save();
 		res.status(201).json({ user: newUser });
 		return;
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+router.post('/login', async (req, res) => {
+	try {
+		const { email, password } = req.body;
+		console.log(email, password);
+
+		if (!email || !password) {
+			res.status(400).json({ message: 'Enter Email and password!' });
+			return;
+		}
+
+		const isUser = await User.findOne({ email });
+		if (!isUser) {
+			console.log('hi in ');
+			res.status(400).json({ message: 'Email or password is invalid!' });
+			return;
+		}
+
+		console.log(isUser);
+		console.log(password);
+
+		if (isUser.password !== password) {
+			console.log('password not match');
+			console.log(isUser.password);
+			res.status(400).json({ message: 'Email or password is invalid!' });
+			return;
+		}
+
+		res.status(200).json({ user: isUser });
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
