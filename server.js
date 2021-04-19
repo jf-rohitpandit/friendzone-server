59bc23fd0f4bb2f7e0f9f33b396b2e11501b62f5
Rohit Pandit <rohit0311@outlook.com>
2021-04-19 08:26:30 +0530

    adds morgan cors and body-parser in server.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 require('./db.js');
 
