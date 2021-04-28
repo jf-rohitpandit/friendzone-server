4629091b15d4e81a1c216b5ea4ab8bba181fb0d8
Rohit Pandit <rohit0311@outlook.com>
2021-04-28 16:20:59 +0530

    adds faker in the home route to send dummy data

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const faker = require('faker');
 
 router.get('/', (req, res) => {
-	res.send('hi');
+	const image = faker.image.avatar();
+	const name = faker.name.firstName() + faker.name.lastName();
+	const country = faker.address.country();
+	const age = Math.floor(Math.random() * 20 + 18);
+	const aboutMe = faker.lorem.lines();
+	const gender = faker.name.gender();
+
+	res
+		.status(200)
+		.json({ data: { image, name, age, gender, aboutMe, country } });
 });
 
 module.exports = router;
