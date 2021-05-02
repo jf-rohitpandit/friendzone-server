f71f1f9178c65a50916f6edbe2f7b3312f30f6f1
Rohit Pandit <rohit0311@outlook.com>
2021-05-02 21:43:16 +0530

    adds friendList data without photo

--- routes/friend.js
+++ routes/friend.js
@@ -4,7 +4,6 @@ const User = require('../models/userModel');
 
 router.put('/', async (req, res) => {
 	try {
-		console.log('hi in the friend router');
 		const id = req.userId;
 		const friendId = req.body.friendId;
 
@@ -21,6 +20,31 @@ router.put('/', async (req, res) => {
 		res.status(200).json({ message: 'success' });
 	} catch (error) {
 		console.log(error);
+		res.status(500).json({ error: error });
+	}
+});
+
+router.get('/', async (req, res) => {
+	try {
+		const id = req.userId;
+		const user = await User.findOne({ _id: id });
+
+		console.log(user.friend);
+
+		const list = [];
+
+		for (let i = 0; i < user.friend.length; i++) {
+			const tempUser = await User.findOne({ _id: user.friend[i] });
+			let temp = {
+				name: tempUser.name,
+				id: tempUser.id,
+			};
+
+			list.push(temp);
+		}
+
+		res.status(200).json({ list: list });
+	} catch (error) {
 		res.status(500).json({ error: error });
 	}
 });
